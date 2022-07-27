55b2b59cb58c60955a7059b6854facb93c560681
LadyAlena <107322107+LadyAlena@users.noreply.github.com>
2022-07-27 18:59:20 +0300

    Changed task 3

--- 01/Source.cpp
+++ 01/Source.cpp
@@ -3,33 +3,36 @@
 
 #define MODE 1
 
-#if MODE == 1
-	#include "add.h"
-#endif
-
-int main(int argc, char** argv) {
-
-	setlocale(LC_ALL, "ru");
-
 #ifndef MODE
 
 #error Macro MOOD required.
 
 #endif
 
+int main(int argc, char** argv) {
+
+	setlocale(LC_ALL, "ru");
+
 #if MODE == 1
+	#include "add.h"
+
+	std::cout << "ÃÃ Ã¡Ã®Ã²Ã Ã¾ Ã¢ Ã¡Ã®Ã¥Ã¢Ã®Ã¬ Ã°Ã¥Ã¦Ã¨Ã¬Ã¥" << std::endl;
+	
 	int a = 0;
 	int b = 0;
-	std::cout << "Ðàáîòàþ â áîåâîì ðåæèìå" << std::endl;
-	std::cout << "Ââåäèòå ÷èñëî 1: ";
+	
+	std::cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã·Ã¨Ã±Ã«Ã® 1: ";
 	std::cin >> a;
-	std::cout << "Ââåäèòå ÷èñëî 2: ";
+	
+	std::cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã·Ã¨Ã±Ã«Ã® 2: ";
 	std::cin >> b;
-	std::cout << "Ðåçóëüòàò ñëîæåíèÿ: " << add(a, b);
+	
+	std::cout << "ÃÃ¥Ã§Ã³Ã«Ã¼Ã²Ã Ã² Ã±Ã«Ã®Ã¦Ã¥Ã­Ã¨Ã¿: " << add(a, b);
+	
 #elif MODE == 0
-	std::cout << "Ðàáîòàþ â ðåæèìå òðåííèðîâêè" << std::endl;
+	std::cout << "ÃÃ Ã¡Ã®Ã²Ã Ã¾ Ã¢ Ã°Ã¥Ã¦Ã¨Ã¬Ã¥ Ã²Ã°Ã¥Ã­Ã­Ã¨Ã°Ã®Ã¢ÃªÃ¨" << std::endl;
 #else 
-	std::cout << "Íåèçâåñòíûé ðåæèì. Çàâåðøåíèå ðàáîòû" << std::endl;
+	std::cout << "ÃÃ¥Ã¨Ã§Ã¢Ã¥Ã±Ã²Ã­Ã»Ã© Ã°Ã¥Ã¦Ã¨Ã¬. Ã‡Ã Ã¢Ã¥Ã°Ã¸Ã¥Ã­Ã¨Ã¥ Ã°Ã Ã¡Ã®Ã²Ã»" << std::endl;
 #endif
 
 	return 0;
