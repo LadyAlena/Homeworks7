9498f10ccaffae0610dc5d2223d072ff7d6916f7
Elena Stepanova <lady.alena-he2015@yandex.ru>
2022-07-27 16:14:00 +0300

    Task 1 added

---
+++ 01/Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+#define MODE 1
+
+#if MODE == 1
+	#include "add.h"
+#endif
+
+int main(int argc, char** argv) {
+
+	setlocale(LC_ALL, "ru");
+
+#ifndef MODE
+
+#error Macro MOOD required.
+
+#endif
+
+#if MODE == 1
+	int a = 0;
+	int b = 0;
+	std::cout << "Работаю в боевом режиме" << std::endl;
+	std::cout << "Введите число 1: ";
+	std::cin >> a;
+	std::cout << "Введите число 2: ";
+	std::cin >> b;
+	std::cout << "Результат сложения: " << add(a, b);
+#elif MODE == 0
+	std::cout << "Работаю в режиме треннировки" << std::endl;
+#else 
+	std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+#endif
+
+	return 0;
+}
